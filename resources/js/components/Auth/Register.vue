f12f5267435887bbfe00d003d51a9bf924067649
Grigory Gerasimov <grigorygerasimov@Grigorys-MacBook-Pro.local>
2023-07-09 18:55:38 +0200

    auth register implemented

--- resources/js/components/Auth/Register.vue
+++ resources/js/components/Auth/Register.vue
@@ -1,11 +1,65 @@
 <script>
 import { defineComponent } from 'vue'
-import { FormControl, Button } from '../Common/'
+import { FormControl, Button, ErrorMessage } from '../Common/'
+import { SanctumService } from '../../services/SanctumService.js'
 
 export default defineComponent({
     name: 'Register',
+
     components: {
-        FormControl, Button
+        ErrorMessage,
+        FormControl,
+        Button
+    },
+
+    data() {
+        return {
+            firstName: null,
+            lastName: null,
+            email: null,
+            password: null,
+            passwordConfirmation: null,
+            errorData: null
+        }
+    },
+
+    computed: {
+        userData() {
+            return {
+                name: `${this.firstName} ${this.lastName}`,
+                email: this.email,
+                password: this.password,
+                password_confirmation: this.passwordConfirmation,
+            }
+        },
+
+        isDisabled() {
+            return !(
+                this.firstName &&
+                this.lastName &&
+                this.email &&
+                this.password &&
+                this.passwordConfirmation
+            ) || this.errorData
+        }
+    },
+
+    methods: {
+        async handleSubmit() {
+            try {
+                await axios.get('/sanctum/csrf-cookie')
+                const response = await axios.post('/register', this.userData)
+                SanctumService.storeToken(response.config.headers['X-XSRF-TOKEN'])
+                this.$router.replace({ name: 'main' })
+            } catch (error) {
+                this.errorData = Object.values(error.response.data?.errors).flat(Infinity)
+                setTimeout(() => this.errorData = null, 3000)
+            }
+        }
+    },
+
+    mounted() {
+        this.$refs.init.$el.children[1].focus()
     }
 })
 </script>
@@ -18,6 +72,8 @@ export default defineComponent({
             model-name='first_name'
             model-id='first_name'
             model-placeholder='First Name'
+            v-model='firstName'
+            ref='init'
         />
         <FormControl
             model-label='Last Name'
@@ -25,6 +81,7 @@ export default defineComponent({
             model-name='last_name'
             model-id='last_name'
             model-placeholder='Last Name'
+            v-model='lastName'
         />
         <FormControl
             model-label='Email'
@@ -32,6 +89,7 @@ export default defineComponent({
             model-name='email'
             model-id='email'
             model-placeholder='Email Address'
+            v-model='email'
         />
         <FormControl
             model-label='Password'
@@ -39,6 +97,7 @@ export default defineComponent({
             model-name='password'
             model-id='password'
             model-placeholder='Password'
+            v-model='password'
         />
         <FormControl
             model-label='Password Confirmation'
@@ -46,10 +105,16 @@ export default defineComponent({
             model-name='password_confirmation'
             model-id='password_confirmation'
             model-placeholder='Confirm Password'
+            v-model='passwordConfirmation'
         />
-        <Button class='btn-outline-dark'>
+        <Button
+            class='btn-outline-dark'
+            @click.prevent='handleSubmit'
+            :disabled='isDisabled'
+        >
             Submit
         </Button>
+        <ErrorMessage v-for='(error, index) in errorData' :key='index' :msg='error'/>
     </div>
 </template>
 
